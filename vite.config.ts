223dc26ce018e7204b0a18b260dc07c59705e8ac
Gibson Waheire Giteru <pwriter455@gmail.com>
2025-08-14 22:10:51 +0300

    chore: update package dependencies, rename project, and remove postcss config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 // Note: using Tailwind via PostCSS (postcss.config.js). Remove v4 vite plugin.
 import { fileURLToPath, URL } from 'node:url'
 
